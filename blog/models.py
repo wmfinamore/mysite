74a1cb90084da73535b1b689f8c21b2e7dd60418
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-10-22 00:11:03 -0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250, help_text='post title')
+    slug = models.SlugField(max_length=250, unique_for_date='publish',
+                            help_text='short label to be used in URL')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts',
+                               help_text='user that wrote the post')
+    body = models.TextField(help_text='body of the post')
+    publish = models.DateTimeField(default=timezone.now,
+                                   help_text='when the post was published')
+    created = models.DateTimeField(auto_now_add=True,
+                                   help_text='when the post was created')
+    update = models.DateTimeField(auto_now=True, help_text='last time that the post was updated')
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft',
+                              help_text='status of a post')
+
+    class Meta:
+        """sort results by the publish field in descending order by default"""
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
