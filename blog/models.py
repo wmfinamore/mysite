61453424cbf684f694b6559427f45031e2ae50fa
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-10-26 19:18:14 -0300

    Creating a custom manager. Understanding the Django ORM

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -24,6 +30,8 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft',
                               help_text='status of a post')
+    objects = models.Manager()#The default manager
+    published = PublishedManager()#Our custom manager
 
     class Meta:
         """sort results by the publish field in descending order by default"""
@@ -31,3 +39,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
