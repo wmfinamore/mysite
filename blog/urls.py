72e93401f2c30f05aa61cb287068f5810fd2d414
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-11-01 21:42:37 -0300

    Refactoring the project to use class based view

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_detail
+from .views import PostListView
 
 
 app_name = 'blog'
 urlpatterns = [
     # post views
-    path('', post_list, name='post_list'),
+    # path('', post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          post_detail,
          name='post_detail')
