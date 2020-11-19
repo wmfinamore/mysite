16b97458c265ad1b2320f4161921cce51b85d869
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-11-19 17:40:22 -0300

    Using the markdown with templates custom tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,9 +1,16 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 
 register = template.Library()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 @register.simple_tag
